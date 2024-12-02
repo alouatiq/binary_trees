bf886e191ae4864525ebb425e214faf76f9b5c9b
HajarElMannani <laso125@hotmail.com>
2024-12-02 13:47:34 -0800

    task 8- First commit

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_postorder - function that goes through a binary
+ * tree using post-order traversal
+ *@tree: pointer to the root node of the tree to traverse
+ *@func: pointer to a function to call for each node.
+ *Return: Nothing.
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (func == NULL || tree == NULL)
+return;
+/*recursively go left-right-root*/
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+/*execute function for current leaf*/
+func(tree->n);
+}
