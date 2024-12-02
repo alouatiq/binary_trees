37620d775998df30a065f5ccc0b3eec5d7eeb19b
HajarElMannani <laso125@hotmail.com>
2024-12-02 11:42:30 -0800

    Betty styling

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_insert_left - function that inserts a node as
+ *binary_tree_insert_right - function that inserts a node as
  * the left-child of another node
  *@parent: pointer to the node to insert the left-child in
  *@value: value to store in the new node
