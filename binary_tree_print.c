5eb777df1adb62ac18d0b4850dc74f323e7cee9d
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-03 15:42:44 +0100

    Create binary_tree_print.c

---
+++ binary_tree_print.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/* Placeholder implementation for binary_tree_print */
+void binary_tree_print(const binary_tree_t *tree)
+{
+    if (!tree)
+        return;
+
+    printf("%d\n", tree->n);
+    if (tree->left)
+        binary_tree_print(tree->left);
+    if (tree->right)
+        binary_tree_print(tree->right);
+}
