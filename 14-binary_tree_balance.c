ef8abc6280c9da332d4bb2a569315b75421de015
HajarElMannani <laso125@hotmail.com>
2024-12-03 12:52:25 -0800

    betty styling

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height;
-	size_t right_height;
-	if (tree == NULL)
-	return (0);
+size_t left_height;
+size_t right_height;
+if (tree == NULL)
+return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+return ((left_height > right_height ? left_height : right_height) +1);
 }
 
 /**
@@ -29,15 +29,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        int left_height;
-	int right_height;
-	if (tree == NULL)
-	return (0);
+int left_height;
+int right_height;
+if (tree == NULL)
+return (0);
+left_height = tree->left ? (int)binary_tree_height(tree->left) : 0;
+right_height = tree->right ? (int)binary_tree_height(tree->right) : 0;
 
-	left_height = tree->left ? (int)binary_tree_height(tree->left) : 0;
-	
-	right_height = tree->right ? (int)binary_tree_height(tree->right) : 0;
-
-	return (left_height - right_height);
+return (left_height - right_height);
 }
-
