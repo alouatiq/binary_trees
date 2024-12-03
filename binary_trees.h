34b3dbb357a461aaf1a730710e5f78fa848d1659
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-03 16:47:00 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree); /* task 12 */
 size_t binary_tree_nodes(const binary_tree_t *tree); /* task 13 */
 int binary_tree_balance(const binary_tree_t *tree); /* task 14 */
 int binary_tree_is_full(const binary_tree_t *tree); /* task 15 */
+int binary_tree_is_perfect(const binary_tree_t *tree); /* task 16 */
 
 
 
