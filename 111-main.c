d1117de9eed002698ab030b3dfc568ec759b8e64
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-03 17:54:04 +0100

    Create 111-main.c

---
+++ 111-main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point to test bst_insert
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    bst_t *root;
+    bst_t *node;
+
+    root = NULL;
+    node = bst_insert(&root, 98);
+    printf("Inserted: %d\n", node->n);
+    binary_tree_print(root);
+
+    node = bst_insert(&root, 402);
+    printf("\nInserted: %d\n", node->n);
+    binary_tree_print(root);
+
+    node = bst_insert(&root, 12);
+    printf("\nInserted: %d\n", node->n);
+    binary_tree_print(root);
+
+    node = bst_insert(&root, 46);
+    printf("\nInserted: %d\n", node->n);
+    binary_tree_print(root);
+
+    node = bst_insert(&root, 128);
+    printf("\nInserted: %d\n", node->n);
+    binary_tree_print(root);
+
+    node = bst_insert(&root, 402);
+    printf("\nNode should be nil -> %p\n", (void *)node);
+
+    return (0);
+}
