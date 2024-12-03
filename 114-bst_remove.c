54ac2b70efefda791b2ce2af67e35268e99f2687
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-03 17:56:08 +0100

    Create 114-bst_remove.c

---
+++ 114-bst_remove.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * min_value_node - Finds the node with the smallest value in a BST.
+ * @node: Pointer to the root node of the tree to search.
+ *
+ * Return: Pointer to the node with the smallest value.
+ */
+bst_t *min_value_node(bst_t *node)
+{
+    bst_t *current = node;
+
+    while (current && current->left)
+        current = current->left;
+
+    return (current);
+}
+
+/**
+ * bst_remove - Removes a node with a specific value from a BST.
+ * @root: Pointer to the root node of the tree.
+ * @value: Value of the node to remove.
+ *
+ * Return: Pointer to the new root of the tree.
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+    if (root == NULL)
+        return (NULL);
+
+    if (value < root->n)
+        root->left = bst_remove(root->left, value);
+    else if (value > root->n)
+        root->right = bst_remove(root->right, value);
+    else
+    {
+        if (root->left == NULL)
+        {
+            bst_t *temp = root->right;
+            free(root);
+            return (temp);
+        }
