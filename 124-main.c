23716aab8dd3678d191aa8ec2826795347fc97ee
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-04 11:13:49 +0100

    Update 124-main.c

--- 124-main.c
+++ 124-main.c
@@ -1,43 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * print_array - Prints an array of integers
+ * main - Entry point for testing sorted_array_to_avl
  *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    for (i = 0; i < size; i++)
-        printf("%d ", array[i]);
-    printf("\n");
-}
-
-/**
- * main - Entry point
- *
- * Return: 0 on success, error code on failure
+ * Return: Always 0
  */
 int main(void)
 {
     avl_t *tree;
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int array[] = {1, 2, 3, 4, 5, 6, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
-
-    printf("Array: ");
-    print_array(array, n);
 
     tree = sorted_array_to_avl(array, n);
     if (!tree)
         return (1);
 
-    printf("AVL tree:\n");
     binary_tree_print(tree);
-    binary_tree_delete(tree);
-
     return (0);
 }
