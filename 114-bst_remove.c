f3489ad16175a423c7a422e28d58d1e7e12445ce
HajarElMannani <laso125@hotmail.com>
2024-12-04 04:45:58 -0800

    test

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -25,6 +25,7 @@ bst_t *min_value_node(bst_t *node)
 	*/
 bst_t *bst_remove(bst_t *root, int value)
 {
+	bst_t *temp;
 	if (root == NULL)
 	return (NULL);
 
@@ -36,18 +37,18 @@ bst_t *bst_remove(bst_t *root, int value)
 	{
 	if (root->left == NULL)
 	{
-	bst_t *temp = root->right;
+	temp = root->right;
 	free(root);
 	return (temp);
 	}
 	else if (root->right == NULL)
 	{
-	bst_t *temp = root->left;
+	temp = root->left;
 	free(root);
 	return (temp);
 	}
 
-	bst_t *temp = min_value_node(root->right);
+	temp = min_value_node(root->right);
 	root->n = temp->n;
 	root->right = bst_remove(root->right, temp->n);
 	}
