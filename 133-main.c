088ac102f6333ee5b1d86640ba5694f10e83cf7a
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-05 21:57:31 +0100

    Update 133-main.c

--- 133-main.c
+++ 133-main.c
@@ -3,38 +3,37 @@
 #include "binary_trees.h"
 
 /**
-	* main - Entry point
-	*
-	* Return: 0 on success, error code on failure
-	*/
+ * main - Entry point to test heap_extract
+ *
+ * Return: 0 on success, 1 on failure
+ */
 int main(void)
 {
-	heap_t *tree;
-	int array[] = {
-	79, 47, 68, 87, 84, 91, 21, 32, 34, 2,
-	20, 22, 98, 1, 62, 95
-	};
-	size_t n = sizeof(array) / sizeof(array[0]);
-	int extract;
+    heap_t *tree;
+    int array[] = {
+        79, 47, 68, 87, 84, 91, 21, 32, 34, 2,
+        20, 22, 98, 1, 62, 95
+    };
+    size_t n = sizeof(array) / sizeof(array[0]);
+    int extract;
 
-	tree = array_to_heap(array, n);
-	if (!tree)
-	return (1);
+    tree = array_to_heap(array, n);
+    if (!tree)
+        return (1);
+    binary_tree_print(tree);
 
-	binary_tree_print(tree);
+    extract = heap_extract(&tree);
+    printf("Extracted: %d\n", extract);
+    binary_tree_print(tree);
 
-	extract = heap_extract(&tree);
-	printf("Extracted: %d\n", extract);
-	binary_tree_print(tree);
+    extract = heap_extract(&tree);
+    printf("Extracted: %d\n", extract);
+    binary_tree_print(tree);
 
-	extract = heap_extract(&tree);
-	printf("Extracted: %d\n", extract);
-	binary_tree_print(tree);
+    extract = heap_extract(&tree);
+    printf("Extracted: %d\n", extract);
+    binary_tree_print(tree);
 
-	extract = heap_extract(&tree);
-	printf("Extracted: %d\n", extract);
-	binary_tree_print(tree);
-
-	binary_tree_delete(tree);
-	return (0);
+    binary_tree_delete(tree);
+    return (0);
 }
