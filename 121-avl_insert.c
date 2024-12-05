b33e8947edb53a1a07a3d76000ffb14743aa438d
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-12-05 22:21:52 +0100

    Update 121-avl_insert.c
    Single File: Everything is contained in 121-avl_insert.c.
    Betty Compliant: The main avl_insert function now has fewer than 40 lines. Logic is split into two smaller functions (insert_node and rebalance_tree).

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,61 +1,89 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-/**
-	* avl_insert - Inserts a value into an AVL tree.
-	* @tree: Double pointer to the root node of the tree for insertion.
-	* @value: Value to insert.
-	*
-	* Return: Pointer to the created node, or NULL on failure.
-*/
 
+/**
+ * insert_node - Helper function to insert a node into the tree.
+ * @tree: Double pointer to the root node of the tree for insertion.
+ * @value: Value to insert.
+ *
+ * Return: Pointer to the created node, or NULL on failure.
+ */
+avl_t *insert_node(avl_t **tree, int value)
+{
+    avl_t *node = NULL;
+
+    if (value < (*tree)->n)
+    {
+        if ((*tree)->left == NULL)
+        {
+            (*tree)->left = binary_tree_node(*tree, value);
+            node = (*tree)->left;
+        }
+        else
+            node = insert_node((avl_t **)&(*tree)->left, value);
+    }
+    else if (value > (*tree)->n)
+    {
+        if ((*tree)->right == NULL)
+        {
+            (*tree)->right = binary_tree_node(*tree, value);
+            node = (*tree)->right;
+        }
+        else
+            node = insert_node((avl_t **)&(*tree)->right, value);
+    }
+    return (node);
+}
+
+/**
+ * rebalance_tree - Helper function to rebalance the AVL tree.
+ * @tree: Double pointer to the root node of the tree.
+ * @value: Value that caused imbalance.
+ */
+void rebalance_tree(avl_t **tree, int value)
+{
+    int balance = binary_tree_balance(*tree);
+
+    if (balance > 1 && value < (*tree)->left->n)
+        *tree = binary_tree_rotate_right(*tree);
+    else if (balance < -1 && value > (*tree)->right->n)
+        *tree = binary_tree_rotate_left(*tree);
+    else if (balance > 1 && value > (*tree)->left->n)
+    {
+        (*tree)->left = binary_tree_rotate_left((*tree)->left);
+        *tree = binary_tree_rotate_right(*tree);
+    }
+    else if (balance < -1 && value < (*tree)->right->n)
+    {
+        (*tree)->right = binary_tree_rotate_right((*tree)->right);
+        *tree = binary_tree_rotate_left(*tree);
+    }
+}
+
+/**
+ * avl_insert - Inserts a value into an AVL tree.
+ * @tree: Double pointer to the root node of the tree for insertion.
+ * @value: Value to insert.
+ *
+ * Return: Pointer to the created node, or NULL on failure.
+ */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	int balance;
-	avl_t *node = NULL;
+    avl_t *node;
 
-	if (!tree)
-	return (NULL);
-	if (!*tree)
-	{ /* If the tree is empty */
-	*tree = binary_tree_node(NULL, value);
-	return (*tree);
-	}
-	if (value < (*tree)->n)
-	{
-	if ((*tree)->left == NULL)
-	{
-	(*tree)->left = binary_tree_node(*tree, value);
-	node = ((*tree)->left);
-	}
-	else
-	node = avl_insert((avl_t **)&(*tree)->left, value);
-	}
-	else if (value > (*tree)->n)
-	{
-	if ((*tree)->right == NULL)
-	{
-	(*tree)->right = binary_tree_node(*tree, value);
-	node = (*tree)->right;
-	}
-	else
-	node = avl_insert((avl_t **)&(*tree)->right, value);
-	}
-	else
-	return (NULL); /* Duplicates not allowed */
-	balance = binary_tree_balance(*tree);
-	if (balance > 1 && value < (*tree)->left->n)
-	*tree = binary_tree_rotate_right(*tree);
-	else if (balance < -1 && value > (*tree)->right->n)
-	*tree = binary_tree_rotate_left(*tree);
-	else if (balance > 1 && value > (*tree)->left->n)
-	{
-	(*tree)->left = binary_tree_rotate_left((*tree)->left);
-	*tree = binary_tree_rotate_right(*tree);
-	}
-	else if (balance < -1 && value < (*tree)->right->n)
-	{
-	(*tree)->right = binary_tree_rotate_right((*tree)->right);
-	*tree = binary_tree_rotate_left(*tree);
-	}
-	return (node ? node : *tree);
+    if (!tree)
+        return (NULL);
+
+    if (!*tree)
+    {
+        *tree = binary_tree_node(NULL, value);
+        return (*tree);
+    }
+
+    node = insert_node(tree, value);
+    if (!node)
+        return (NULL);
+
+    rebalance_tree(tree, value);
+    return (node);
 }
